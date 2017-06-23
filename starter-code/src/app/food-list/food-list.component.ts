099b08f945c084dfecf99d8f241bdc9b00f6d436
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-06-22 20:56:47 -0400

    Food creation added

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -9,11 +9,34 @@ import foodsList from '../foods';
 export class FoodListComponent implements OnInit {
   foods              : Object[];
   pattern            : string;
-
+  isEditing          : boolean = false;
+  newFoodName        : string = "Example Name";
+  newFoodCalories    : number = 250;
+  newFoodImage       : string = "";
   constructor() { }
 
   ngOnInit() {
     this.foods = foodsList;
   }
+
+  enableUserToAddFood() {
+    this.isEditing = !this.isEditing;
+  }
+
+  newFood(){
+    const newFood = {
+      name: this.newFoodName,
+      calories: this.newFoodCalories,
+      image: this.newFoodImage,
+      quantity: 0
+    }
+
+  this.foods.unshift(newFood);
+
+  this.isEditing = false;
+  this.newFoodName = "";
+  this.newFoodCalories = null;
+  this.newFoodImage = "";
+}
 
 }
