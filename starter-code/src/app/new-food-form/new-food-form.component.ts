66718e7506d0944d34e11ccc03c1299f55ae35a4
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-06-24 10:57:32 -0400

    ngForm submit

--- starter-code/src/app/new-food-form/new-food-form.component.ts
+++ starter-code/src/app/new-food-form/new-food-form.component.ts
@@ -6,14 +6,26 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./new-food-form.component.css']
 })
 export class NewFoodFormComponent implements OnInit {
-  @Output() onAddFood = new EventEmitter<object>();
+  // OnAddFood is a custom event we just invented
+  @Output() onAddFood = new EventEmitter<Object>();
   newFoodName : string = "Example";
   newFoodCalories  : number = 250;
   newFoodImage     : string = "http://dhiglobal.com/wp-content/uploads/2016"
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  }
+
+  submitForm(myForm) {
+    const newFood = {
+      name: this.newFoodName,
+      calories: this.newFoodCalories,
+      image: this.newFoodImage,
+      quantity: 0
+    };
+    console.log(myForm);
+   // triggering our custom, invented, onAddFood event
+    this.onAddFood.emit(newFood);
   }
 
 }
